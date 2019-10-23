ce58d818c345e16d601ab38bd43b47af0d63ec97
Dogyun Jeong <dogyunjeong@gmail.com>
2019-10-23 19:00:58 +0200

    Initial commit of static website generator

---
+++ static-generator/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <DefaultTitleBar />
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <span>&copy; \{{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import DefaultTitleBar from '~/components/titleBars/DefaultTitleBar/DefaultTitleBar.vue'
+
+export default {
+  components: {
+    DefaultTitleBar,
+  },
+}
+</script>
---
+++ static-generator/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <DefaultStaticText />
+</template>
+
+<script>
+import DefaultStaticText from '~/components/staticTexts/DefaultStaticText/DefaultStaticText.vue'
+export default {
+  components: {
+    DefaultStaticText,
+  }
+}
+</script>
