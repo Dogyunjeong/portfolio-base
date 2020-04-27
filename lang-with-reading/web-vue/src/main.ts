a3bba24fee4ba4b955d9ff011b0b62c0acf0bba4
Dogyun Jeong <dogun098@gmail.com>
2020-04-27 23:07:46 +0900

    build: add vuetify

--- lang-with-reading/web-vue/src/main.ts
+++ lang-with-reading/web-vue/src/main.ts
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import './plugins/axios'
-import './plugins/vuetify'
 import App from './App.vue'
 import router from './router/router'
 import store from './store'
 import './registerServiceWorker'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
